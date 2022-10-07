9c362e85668a3e11dc65f2ed3648996e80e5252d
darsh <darsh@DESKTOP-HM8QHNT>
2022-10-07 09:50:10 +0530

    UI Code Added

--- ebm/src/main/java/com/cs/ebm/utils/PasswordUtil.java
+++ ebm/src/main/java/com/cs/ebm/utils/PasswordUtil.java
@@ -1,5 +1,24 @@
 package com.cs.ebm.utils;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class PasswordUtil {
+
+	public static String getPasswordInMD5(String input) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			byte[] messageDigest = md.digest(input.getBytes());
+			BigInteger no = new BigInteger(1, messageDigest);
+			String hashtext = no.toString(16);
+			while (hashtext.length() < 32) {
+				hashtext = "0" + hashtext;
+			}
+			return hashtext;
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 }
