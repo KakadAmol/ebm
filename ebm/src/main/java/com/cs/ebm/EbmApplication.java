5cbfad0fa6a9df6dd8a89d13d81ac04cb790cc76
darshit <darshit.rabadiya@hotmail.com>
2023-02-10 10:06:42 +0530

    Logger Changes Added

--- ebm/src/main/java/com/cs/ebm/EbmApplication.java
+++ ebm/src/main/java/com/cs/ebm/EbmApplication.java
@@ -1,13 +1,18 @@
 package com.cs.ebm;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EbmApplication {
-
+	
+	private static Logger log = LogManager.getLogger(EbmApplication.class); 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EbmApplication.class, args);
+		log.info("Welcome to EBM Application");
 	}
 
 }
